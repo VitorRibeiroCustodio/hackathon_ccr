6726ba0a0def84f991aaa125ebc99ebda867b540
VitorRibeiroCustodio <vitorribeiro221196@gmail.com>
2020-06-13 22:57:10 -0300

    start profile

--- src/Screens/Profile.tsx
+++ src/Screens/Profile.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ListItem } from 'react-native-elements'
 
 import { AppBar } from '../Components/Appbar';
+
+const list = [
+  {
+    title: 'Histórico de doações',
+    icon: 'av-timer'
+  },
+  {
+    title: 'Alterar senha',
+    icon: 'flight-takeoff'
+  },
+]
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <View>
-        <Text>Profile Screen</Text>
+      <View style={styles.listContainer}>
+        {
+          list.map((item, i) => (
+            <ListItem
+              key={i}
+              title={item.title}
+              leftIcon={{ name: item.icon }}
+              bottomDivider
+              chevron
+            />
+          ))
+        }
       </View>
     </View>
   );
@@ -20,5 +42,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  listContainer: {
+    width: 300,
+    height: 200,
   },
 });
